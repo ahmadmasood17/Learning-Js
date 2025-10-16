b9528b215493c32773f0945b35a9cd66a157a84a
tekxai125 <developer@tekxai.com>
2025-10-16 18:10:42 +0500

    comparison

---
+++ comparison.js
@@ -0,0 +1,6 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+// Jab JavaScript me comparison (>, <) hota hai,to null number me convert hota hai.
--- oprations.js
+++ oprations.js
@@ -1,3 +1,37 @@
-console.log(2**3);
-
+// console.log(2**3);
+                                                              
 //2 ** 3  2 × 2 × 2      8
+
+// console.log(2%3);  
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 4)
+// console.log(1 + 2 + "4")
+// console.log(true)
+// console.log(+true)
+// console.log(-true)
+
+// ++counter name prifix counter++ name  post fix
+
+
+
+let x = 3;
+const y = x++;
+//idr icrement value inital hony k bad hoga is liye iski value 3 h rahy gi 
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+//idr icrement value inital hony se pehle  hoga is liye iski value 4 h rahy gi
+
+console.log(`a:${a}, b:${b}`);
+
+
+
+
+
+
