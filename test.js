167832c552f812c65f18c6f18490814471af8876
tekxai125 <developer@tekxai.com>
2025-10-16 12:48:05 +0500

    add typeof

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
-console.log("helo");
+// "use strict";
+
+// let name = "Ahmad";
+// var email = "ahmad@gmail.com";
+// const password = "123123";
+
+// text.email,
+
+// email = 2;
+// console.log(email, password, text);
+
+//    {   }  this brackets scope
+
+//var scope problem ha value change hojati thi ab let hai
+//not use var because
+//undifined variable declare kiya hai usko value ni di 
+//null ek type
+//symbol uniquenes
+
+
+let un = undefined;
+let nu = null;
+
+
+let an = Number(un)
+
+console.log(typeof an);
+console.log(typeof nu);
+
+console.log( nu);
+
+
+
